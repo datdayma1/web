a7d78aafbc869a1fa7d50620d584298a5bb4a4e6
datdayma1 <hathedat2k3@gmail.com>
2024-10-09 18:06:07 +0700

    Lop.php

--- App/Models/Lop.php
+++ App/Models/Lop.php
@@ -1,17 +1,25 @@
 <?php 
-    require_once 'App/Core/db.php';
+require_once 'App/Core/db.php';
 
-class Lop extends DB{
-    public function getlop()
-    {
-        $sql = "SELECT * FROM `tbl_lop`";
-        $result = $this->db->query($sql);
-        if(mysqli_num_rows($result) > 0){
-            while($row = mysqli_fetch_assoc($result))
-            {
+class Lop extends DB {
+    public function getlop() {
+        $sql = "SELECT * FROM tbl_lop";
+        $stmt = sqlsrv_query($this->db, $sql);
+        
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));  // In lỗi nếu truy vấn thất bại
+        }
+
+        $data = [];
+        // Kiểm tra xem có hàng dữ liệu nào không
+        if (sqlsrv_has_rows($stmt)) {
+            // Lặp qua các kết quả và lưu vào mảng $data
+            while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
                 $data[] = $row;
             }
         }
+
         return $data;
     }
 }
+?>
