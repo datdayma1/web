cb6d2cc642cbb07838fa858c69a0ca2627fc9bb8
datdayma1 <hathedat2k3@gmail.com>
2024-10-09 18:07:32 +0700

    SinhVien.php

--- App/Models/SinhVien.php
+++ App/Models/SinhVien.php
@@ -1,75 +1,84 @@
 <?php
 require_once 'App/Core/db.php';
-class SinhVien extends DB{
-    public function getsvbylop()
-{
-    $data = [];
-    $sql = "SELECT * FROM tbl_sinhvien";
-    $result = $this->db->query($sql);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $ma_lop = $row['ma_lop'];
-            $lop_sql = "SELECT * FROM tbl_lop WHERE ma_lop = '$ma_lop'";
-            $lop_result = $this->db->query($lop_sql);
-            if(mysqli_num_rows($lop_result) > 0) {
-                while($row_lop = mysqli_fetch_assoc($lop_result)){
-                    $row['ten_lop'] = $row_lop['ten_lop'];
+class SinhVien extends DB {
+    public function getsvbylop() {
+        $data = [];
+        $sql = "SELECT * FROM tbl_sinhvien";
+        $stmt = sqlsrv_query($this->db, $sql);
+
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true)); // Hiển thị lỗi nếu có
+        }
+
+        if (sqlsrv_has_rows($stmt)) {
+            while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+                $ma_lop = $row['ma_lop'];
+                $lop_sql = "SELECT * FROM tbl_lop WHERE ma_lop = ?";
+                $lop_stmt = sqlsrv_query($this->db, $lop_sql, [$ma_lop]);
+
+                if ($lop_stmt && sqlsrv_has_rows($lop_stmt)) {
+                    while ($row_lop = sqlsrv_fetch_array($lop_stmt, SQLSRV_FETCH_ASSOC)) {
+                        $row['ten_lop'] = $row_lop['ten_lop'];
+                    }
                 }
-            }
-            $data[] = $row;
-        }
-    }
-    return $data;
-}
-
-    public function themSV($ten_sv, $ngaysinh, $gioitinh, $ma_lop){
-        $ten_sv = $this->db->real_escape_string($ten_sv);
-        $ngaysinh = $this->db->real_escape_string($ngaysinh);
-        $gioitinh = $this->db->real_escape_string($gioitinh);
-        $ma_lop = $this->db->real_escape_string($ma_lop);
-        $sql = "INSERT INTO tbl_sinhvien (ten_sv, ngaysinh, gioitinh, ma_lop) VALUES ('$ten_sv', '$ngaysinh', '$gioitinh', '$ma_lop')";
-        $result = $this->db->query($sql);
-        if($result){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function getsvbyid($id){
-        $sql = "SELECT * FROM tbl_sinhvien WHERE ma_sv = '$id'";
-        $result = $this->db->query($sql);
-        if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_assoc($result)) {
                 $data[] = $row;
+            }
         }
-        }
+
         return $data;
     }
 
-    public function suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop){
-        $ten_sv = $this->db->real_escape_string($ten_sv);
-        $ngaysinh = $this->db->real_escape_string($ngaysinh);
-        $gioitinh = $this->db->real_escape_string($gioitinh);
-        $ma_lop = $this->db->real_escape_string($ma_lop);
-        $sql = "UPDATE tbl_sinhvien SET ten_sv = '$ten_sv', ngaysinh = '$ngaysinh', gioitinh = '$gioitinh', ma_lop = '$ma_lop' WHERE ma_sv = '$id'";
-        $result = $this->db->query($sql);
-        if($result){
-            return true;
-        } else {
+    public function themSV($ten_sv, $ngaysinh, $gioitinh, $ma_lop) {
+        $sql = "INSERT INTO tbl_sinhvien (ten_sv, ngaysinh, gioitinh, ma_lop) VALUES (?, ?, ?, ?)";
+        $params = [$ten_sv, $ngaysinh, $gioitinh, $ma_lop];
+        $stmt = sqlsrv_query($this->db, $sql, $params);
+
+        if ($stmt === false) {
             return false;
         }
+
+        return true;
     }
 
-    public function xoaSV($id){
-        $sql = "DELETE FROM tbl_sinhvien WHERE ma_sv = '$id'";
-        $result = $this->db->query($sql);
-        if($result){
-            return true;
-        } else {
+    public function getsvbyid($id) {
+        $sql = "SELECT * FROM tbl_sinhvien WHERE ma_sv = ?";
+        $stmt = sqlsrv_query($this->db, $sql, [$id]);
+
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        $data = [];
+        if (sqlsrv_has_rows($stmt)) {
+            while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+                $data[] = $row;
+            }
+        }
+
+        return $data;
+    }
+
+    public function suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop) {
+        $sql = "UPDATE tbl_sinhvien SET ten_sv = ?, ngaysinh = ?, gioitinh = ?, ma_lop = ? WHERE ma_sv = ?";
+        $params = [$ten_sv, $ngaysinh, $gioitinh, $ma_lop, $id];
+        $stmt = sqlsrv_query($this->db, $sql, $params);
+
+        if ($stmt === false) {
             return false;
         }
+
+        return true;
     }
 
+    public function xoaSV($id) {
+        $sql = "DELETE FROM tbl_sinhvien WHERE ma_sv = ?";
+        $stmt = sqlsrv_query($this->db, $sql, [$id]);
+
+        if ($stmt === false) {
+            return false;
+        }
+
+        return true;
+    }
 }
-
+?>
