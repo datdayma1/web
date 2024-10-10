eec24a1e4274b787e9ab233e5e213309f4576dc7
datdayma1 <hathedat2k3@gmail.com>
2024-10-10 10:28:45 +0700

    Update db.php

--- App/Core/db.php
+++ App/Core/db.php
@@ -7,7 +7,30 @@ class DB{
     protected $serverName;
     protected $connectionOptions;
 
+    public function __construct(){
+        $this->serverName = "thi.database.windows.net";
+        
+        // Cấu hình thông tin kết nối
+        $this->connectionOptions = array(
+            "Database" => "sinhvien",     // Tên database
+            "UID" => "thi",               // Tên người dùng
+            "PWD" => "@A123456",          // Mật khẩu
+            "CharacterSet" => "UTF-8"     // Sử dụng mã hóa ký tự UTF-8
+        );
 
+        // Kết nối với SQL Server
+        $this->db = sqlsrv_connect($this->serverName, $this->connectionOptions);
+
+        // Kiểm tra kết nối
+        if($this->db === false){
+            die(print_r(sqlsrv_errors(), true)); // Hiển thị lỗi kết nối nếu có
+        }
+    }
+
+    // Hàm đóng kết nối
+    public function closeConnection(){
+        sqlsrv_close($this->db);
+    }
 }
 
 ?>
