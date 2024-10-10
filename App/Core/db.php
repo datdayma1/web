093149024d7790c08de3c3ee1f42bdfd4f435ebf
datdayma1 <hathedat2k3@gmail.com>
2024-10-10 10:12:23 +0700

    db.php

--- App/Core/db.php
+++ App/Core/db.php
@@ -1,20 +1,30 @@
-<?php 
 class DB{
     public $db;
-    protected $localhost;
-    protected $username;
-    protected $password;
-    protected $dbname;
+    protected $serverName;
+    protected $connectionOptions;
 
     public function __construct(){
-        $this->localhost = "thi.database.windows.net";
-        $this->username = "thi";
-        $this->password = "@A123456";
-        $this->dbname = "sinhvien";
-        $this->db = new mysqli($this->localhost, $this->username, $this->password, $this->dbname);
-        if($this->db->connect_error){
-            die("Connection failed: ". $this->db->connect_error);
+        $this->serverName = "thi.database.windows.net";
+        
+        // Cấu hình thông tin kết nối
+        $this->connectionOptions = array(
+            "Database" => "sinhvien",     // Tên database
+            "UID" => "thi",               // Tên người dùng
+            "PWD" => "@A123456",          // Mật khẩu
+            "CharacterSet" => "UTF-8"     // Sử dụng mã hóa ký tự UTF-8
+        );
+
+        // Kết nối với SQL Server
+        $this->db = sqlsrv_connect($this->serverName, $this->connectionOptions);
+
+        // Kiểm tra kết nối
+        if($this->db === false){
+            die(print_r(sqlsrv_errors(), true)); // Hiển thị lỗi kết nối nếu có
         }
-        mysqli_set_charset($this->db, 'utf8');
+    }
+
+    // Hàm đóng kết nối
+    public function closeConnection(){
+        sqlsrv_close($this->db);
     }
 }
