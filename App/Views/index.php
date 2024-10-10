c1754e85f1a05baf8d58e982bb7bd8a3d4a00d47
datdayma1 <hathedat2k3@gmail.com>
2024-10-10 10:44:18 +0700

    Update index.php

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,1 +1,7 @@
-<?php echo "hêhe" ?>
+<?php
+echo "kiemtra1";
+// Giả sử bạn đã gọi phương thức getDanhSachSinhVien trong Controller để lấy danh sách sinh viên
+$dssv = $controller->getDanhSachSinhVien();
+?>
+
+
