91d40ffe527cfd003a7262a3323cdd57d3eb280d
datdayma1 <hathedat2k3@gmail.com>
2024-10-20 20:23:21 +0700

    ControllerBase.php

--- App/Controllers/ControllerBase.php
+++ App/Controllers/ControllerBase.php
@@ -1,4 +1,83 @@
-<?php 
+// <?php 
+// require_once 'App/Models/SinhVien.php';
+// require_once 'App/Models/Lop.php';
+
+// class ControllerBase {
+//     public function render($view, $params = []) {
+//         extract($params);
+//         require_once 'App/Views/'.$view.'.php';
+//     }
+
+//     public function index() {
+//         $dssv = new SinhVien();
+//         $dssv = $dssv->getsvbylop();
+//         $this->render('index', ['dssv' => $dssv]);
+//     }
+
+//     public function create() {
+//         $lop = new Lop();
+//         $lops = $lop->getlop();
+//         $this->render('create', ['lops' => $lops]);
+//     }
+
+//     public function themSV(){
+//         if(isset($_POST['ten_sv']) && $_POST['ngaysinh'] && $_POST['gioitinh'] && ($_POST['ma_lop']))
+//         {
+//             $ten_sv = $_POST['ten_sv'];
+//             $ngaysinh = $_POST['ngaysinh'];
+//             $gioitinh = $_POST['gioitinh'];
+//             $ma_lop = $_POST['ma_lop'];
+//             $sv = new SinhVien();
+//             if($sv->themSV($ten_sv, $ngaysinh, $gioitinh, $ma_lop)){
+//                 header("Location: index.php");
+//                 exit();
+//             } else {
+//                 echo "Đã xảy ra lỗi khi thêm sinh viên!";
+//             }
+            
+//         }
+//     }
+
+//     public function edit($id) {
+//         $lop = new Lop();
+//         $lops = $lop->getlop();
+//         $sv = new SinhVien();
+//         $sv = $sv->getsvbyid($id);
+//         $sv = !empty($sv) ? $sv[0] : null;
+//         $this->render('edit', ['sv' => $sv, 'lops' => $lops]);
+//     }
+
+//     public function suaSV($id){
+//         if(isset($_POST['ten_sv']) && $_POST['ngaysinh'] && $_POST['gioitinh'] && ($_POST['ma_lop']))
+//         {
+//             $ten_sv = $_POST['ten_sv'];
+//             $ngaysinh = $_POST['ngaysinh'];
+//             $gioitinh = $_POST['gioitinh'];
+//             $ma_lop = $_POST['ma_lop'];
+//             $sv = new SinhVien();
+//             if($sv->suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop)){
+//                 header("Location: index.php");
+//                 exit();
+//             } else {
+//                 echo "Đã xảy ra lỗi khi sửa sinh viên!";
+//             }
+            
+//         }
+//     }
+    
+
+
+//     public function XoaSV($id){
+//         $sv = new SinhVien();
+//         if($sv->xoaSV($id)){
+//             header("Location: index.php");
+//             exit();
+//         } else {
+//             echo "Đã xảy ra lỗi khi xóa sinh viên!";
+//         }
+//     }
+// }
+
 require_once 'App/Models/SinhVien.php';
 require_once 'App/Models/Lop.php';
 
@@ -21,20 +100,28 @@ class ControllerBase {
     }
 
     public function themSV(){
-        if(isset($_POST['ten_sv']) && $_POST['ngaysinh'] && $_POST['gioitinh'] && ($_POST['ma_lop']))
-        {
-            $ten_sv = $_POST['ten_sv'];
-            $ngaysinh = $_POST['ngaysinh'];
-            $gioitinh = $_POST['gioitinh'];
-            $ma_lop = $_POST['ma_lop'];
-            $sv = new SinhVien();
-            if($sv->themSV($ten_sv, $ngaysinh, $gioitinh, $ma_lop)){
-                header("Location: index.php");
-                exit();
+        // Kiểm tra phương thức HTTP
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Kiểm tra dữ liệu từ form
+            if (!empty($_POST['ten_sv']) && !empty($_POST['ngaysinh']) && !empty($_POST['gioitinh']) && !empty($_POST['ma_lop'])) {
+                // Làm sạch dữ liệu
+                $ten_sv = htmlspecialchars(trim($_POST['ten_sv']));
+                $ngaysinh = htmlspecialchars(trim($_POST['ngaysinh']));
+                $gioitinh = htmlspecialchars(trim($_POST['gioitinh']));
+                $ma_lop = htmlspecialchars(trim($_POST['ma_lop']));
+
+                $sv = new SinhVien();
+                if ($sv->themSV($ten_sv, $ngaysinh, $gioitinh, $ma_lop)) {
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    echo "Đã xảy ra lỗi khi thêm sinh viên!";
+                }
             } else {
-                echo "Đã xảy ra lỗi khi thêm sinh viên!";
+                echo "Vui lòng điền đầy đủ thông tin.";
             }
-            
+        } else {
+            echo "Yêu cầu không hợp lệ.";
         }
     }
 
@@ -43,62 +130,40 @@ class ControllerBase {
         $lops = $lop->getlop();
         $sv = new SinhVien();
         $sv = $sv->getsvbyid($id);
-        $sv = !empty($sv) ? $sv[0] : null;
-        $this->render('edit', ['sv' => $sv, 'lops' => $lops]);
+        if (!empty($sv)) {
+            $sv = $sv[0];
+            $this->render('edit', ['sv' => $sv, 'lops' => $lops]);
+        } else {
+            echo "Sinh viên không tồn tại.";
+        }
     }
 
-    // public function suaSV($id){
-    //     if(empty($_POST['ten_sv']) && $_POST['ngaysinh'] && $_POST['gioitinh'] && ($_POST['ma_lop']))
-    //     {
-    //         $ten_sv = $_POST['ten_sv'];
-    //         $ngaysinh = $_POST['ngaysinh'];
-    //         $gioitinh = $_POST['gioitinh'];
-    //         $ma_lop = $_POST['ma_lop'];
-    //         $sv = new SinhVien();
-    //         if($sv->suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop)){
-    //             header("Location: index.php");
-    //             exit();
-    //         } else {
-    //             echo "Đã xảy ra lỗi khi sửa sinh viên!";
-    //         }
-            
-    //     }
-    // }
     public function suaSV($id){
-    // Kiểm tra phương thức yêu cầu HTTP
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Kiểm tra các giá trị từ form
-        if (!empty($_POST['ten_sv']) && !empty($_POST['ngaysinh']) && !empty($_POST['gioitinh']) && !empty($_POST['ma_lop'])) {
-            // Làm sạch dữ liệu đầu vào
-            $ten_sv = htmlspecialchars(trim($_POST['ten_sv']));
-            $ngaysinh = htmlspecialchars(trim($_POST['ngaysinh']));
-            $gioitinh = htmlspecialchars(trim($_POST['gioitinh']));
-            $ma_lop = htmlspecialchars(trim($_POST['ma_lop']));
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (!empty($_POST['ten_sv']) && !empty($_POST['ngaysinh']) && !empty($_POST['gioitinh']) && !empty($_POST['ma_lop'])) {
+                $ten_sv = htmlspecialchars(trim($_POST['ten_sv']));
+                $ngaysinh = htmlspecialchars(trim($_POST['ngaysinh']));
+                $gioitinh = htmlspecialchars(trim($_POST['gioitinh']));
+                $ma_lop = htmlspecialchars(trim($_POST['ma_lop']));
 
-            // Khởi tạo đối tượng SinhVien và gọi phương thức suaSV
-            $sv = new SinhVien();
-            if ($sv->suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop)) {
-                // Nếu cập nhật thành công, chuyển hướng về trang index.php
-                header("Location: index.php");
-                exit();
+                $sv = new SinhVien();
+                if ($sv->suaSV($id, $ten_sv, $ngaysinh, $gioitinh, $ma_lop)) {
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    echo "Đã xảy ra lỗi khi sửa sinh viên!";
+                }
             } else {
-                // Nếu cập nhật thất bại, hiển thị thông báo lỗi
-                echo "Đã xảy ra lỗi khi sửa sinh viên! Vui lòng thử lại.";
+                echo "Vui lòng điền đầy đủ thông tin.";
             }
         } else {
-            // Nếu các giá trị từ form không hợp lệ
-            echo "Vui lòng điền đầy đủ thông tin.";
+            echo "Yêu cầu không hợp lệ.";
         }
-    } else {
-        // Nếu không phải là yêu cầu POST
-        echo "Yêu cầu không hợp lệ.";
     }
-}
-
 
     public function XoaSV($id){
         $sv = new SinhVien();
-        if($sv->xoaSV($id)){
+        if ($sv->xoaSV($id)) {
             header("Location: index.php");
             exit();
         } else {
@@ -106,4 +171,5 @@ class ControllerBase {
         }
     }
 }
-?>
+
+// ?>
